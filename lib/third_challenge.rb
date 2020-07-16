027dc214c3c385e9589f7c104229646e36aa07c1
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-16 06:16:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,7 +6,6 @@ def third_challenge
       :matriarch => {:name =>"Lady Montague", :age =>"54"
         },
       :hero => {:name =>"Romeo", :age => "15" :status =>"alive"
-       
         },
       :hero_friends => []
    }, 
